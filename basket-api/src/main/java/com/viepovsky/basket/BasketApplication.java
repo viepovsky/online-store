dfe04ce28d956d87f1598769b8463a5758a345dd
Oskar R <o.rajzner@gmail.com>
2024-01-23 19:33:02 +0100

    Add new module - Basket

---
+++ basket-api/src/main/java/com/viepovsky/basket/BasketApplication.java
@@ -0,0 +1,11 @@
+package com.viepovsky.basket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasketApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BasketApplication.class, args);
+    }
+}
