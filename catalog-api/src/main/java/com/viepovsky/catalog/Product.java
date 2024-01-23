e5fabca04be1952c6fb784192548888c3d08ae77
Oskar R <o.rajzner@gmail.com>
2024-01-23 17:05:53 +0100

    Add docker-compose with mongodp and mongo_express

--- catalog-api/src/main/java/com/viepovsky/catalog/Product.java
+++ catalog-api/src/main/java/com/viepovsky/catalog/Product.java
@@ -1,6 +1,10 @@
 package com.viepovsky.catalog;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.math.BigDecimal;
 import java.util.Map;
--- catalog-api/src/main/java/com/viepovsky/catalog/ProductCategory.java
+++ catalog-api/src/main/java/com/viepovsky/catalog/ProductCategory.java
@@ -1,5 +1,12 @@
 package com.viepovsky.catalog;
 
 enum ProductCategory {
-
+    CLOTHING,
+    ELECTRONICS,
+    HOME_AND_GARDEN,
+    SPORTS_AND_RECREATION,
+    BOOKS,
+    FOOD_AND_BEVERAGES,
+    HEALTH_AND_BEAUTY,
+    AUTOMOTIVE
 }
