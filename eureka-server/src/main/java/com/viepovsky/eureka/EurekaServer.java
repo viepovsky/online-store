9ec14906a038b2e1471c317d4958668f573007e7
Oskar R <o.rajzner@gmail.com>
2024-01-24 13:22:13 +0100

    Add Eureka Server module

---
+++ eureka-server/src/main/java/com/viepovsky/eureka/EurekaServer.java
@@ -0,0 +1,13 @@
+package com.viepovsky.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+class EurekaServer {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer.class, args);
+    }
+}
